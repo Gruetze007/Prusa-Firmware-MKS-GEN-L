3de4d9018b6be462a84300edbf53bd4189e8b4e6
Gruetze007 <76876318+Gruetze007@users.noreply.github.com>
2021-01-03 17:00:17 +0100

    Add files via upload

---
+++ Firmware/config.h
@@ -0,0 +1,75 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+
+#include "Configuration_prusa.h"
+#include "pins.h"
+
+#if (defined(VOLT_IR_PIN) && defined(IR_SENSOR))
+# define IR_SENSOR_ANALOG
+#endif
+
+//ADC configuration
+// MKS_GEN_L
+#ifdef PINDA_THERMISTOR
+  // MKS_GEN_L with pinda thermistor */
+  #define ADC_CHAN_MSK      0b1110000000000000 //used AD channels bit mask (13 = TEMP_0_PIN, 14 = TEMP_BED_PIN, 15 = TEMP_1_PIN)
+  #define ADC_DIDR_MSK      0b1110000000000000 //AD channels DIDR mask (1 ~ disabled digital input)
+  #define ADC_CHAN_CNT      3         //number of used channels)
+  #define ADC_OVRSAMPL      16        //oversampling multiplier
+  #define ADC_CALLBACK      adc_ready //callback function ()
+#else
+  // MKS_GEN_L without pinda thermistor */
+  #define ADC_CHAN_MSK      0b0110000000000000 //used AD channels bit mask (13 = TEMP_0_PIN, 14 = TEMP_BED_PIN)
+  #define ADC_DIDR_MSK      0b0110000000000000 //AD channels DIDR mask (1 ~ disabled digital input)
+  #define ADC_CHAN_CNT      2         //number of used channels)
+  #define ADC_OVRSAMPL      16        //oversampling multiplier
+  #define ADC_CALLBACK      adc_ready //callback function ()
+#endif // PINDA_THERMISTOR
+
+//SWI2C configuration
+#define SWI2C
+//#define SWI2C_SDA         20 //SDA on P3
+//#define SWI2C_SCL         21 //SCL on P3
+#define SWI2C_A8
+#define SWI2C_DEL         20 //2us clock delay
+#define SWI2C_TMO         2048 //2048 cycles timeout
+
+//PAT9125 configuration
+#define PAT9125_SWI2C
+#define PAT9125_I2C_ADDR  0x75  //ID=LO
+//#define PAT9125_I2C_ADDR  0x79  //ID=HI
+//#define PAT9125_I2C_ADDR  0x73  //ID=NC
+#define PAT9125_XRES      0
+#define PAT9125_YRES      240                   // maximum resolution (5*X cpi)
+#define PAT9124_YRES_MM   (5*PAT9125_YRES/25.4) // counts per mm
+
+//SM4 configuration
+#define SM4_DEFDELAY      500       //default step delay [us]
+
+//TMC2130 - Trinamic stepper driver
+//pinout - hardcoded
+//spi:
+#define TMC2130_SPI_RATE       0 // fosc/4 = 4MHz
+#define TMC2130_SPCR           SPI_SPCR(TMC2130_SPI_RATE, 1, 1, 1, 0)
+#define TMC2130_SPSR           SPI_SPSR(TMC2130_SPI_RATE)
+
+//W25X20CL configuration
+// MKS_GEN_L
+#ifdef W25X20CL // W25X20L-Chip on MKS GEN L board does not exist
+  //pinout:
+  #define W25X20CL_PIN_CS        32
+  //spi:
+  #define W25X20CL_SPI_RATE      0 // fosc/4 = 4MHz
+  #define W25X20CL_SPCR          SPI_SPCR(W25X20CL_SPI_RATE, 1, 1, 1, 0)
+  #define W25X20CL_SPSR          SPI_SPSR(W25X20CL_SPI_RATE)
+#endif
+
+//LANG - Multi-language support
+#define LANG_MODE              0 // primary language only
+//#define LANG_MODE              1 // sec. language support
+
+#define LANG_SIZE_RESERVED     0x3000 // reserved space for secondary language (12288 bytes)
+
+
+#endif //_CONFIG_H
---
+++ Firmware/pins_MKS_GEN_L.h
@@ -0,0 +1,134 @@
+/*****************************************************************
+* Arduino RAMPS 1.3/1.4 Pin Assignments
+******************************************************************/
+
+#define ELECTRONICS "MKS_GEN_L"
+
+#define KNOWN_BOARD
+#ifndef __AVR_ATmega2560__
+  #error Oops!  Make sure you have 'Arduino Mega 2560' selected from the 'Tools -> Boards' menu.
+#endif
+
+//#define TMC2130
+//#define UVLO_SUPPORT
+
+//#define AMBIENT_THERMISTOR
+//#define PINDA_THERMISTOR
+
+//#define W25X20CL                 // external 256kB flash
+//#define BOOTAPP                  // bootloader support
+
+//#define IR_SENSOR
+
+#define SWI2C_SDA          20 
+#define SWI2C_SCL          21 
+
+// core pins
+#define X_STEP_PIN         54
+#define X_DIR_PIN          55
+#define X_MIN_PIN           3
+#define X_MAX_PIN           2
+#define X_ENABLE_PIN       38
+#define X_MS1_PIN          -1
+#define X_MS2_PIN          -1
+
+#define Y_STEP_PIN         60
+#define Y_DIR_PIN          61
+#define Y_MIN_PIN          14
+#define Y_MAX_PIN          15
+#define Y_ENABLE_PIN       56
+#define Y_MS1_PIN          -1
+#define Y_MS2_PIN          -1
+
+#define Z_STEP_PIN         46
+#define Z_DIR_PIN          48
+#define Z_MIN_PIN          18
+#define Z_MAX_PIN          19
+#define Z_ENABLE_PIN       62
+#define Z_MAX_PIN          19
+#define Z_MS1_PIN          -1
+#define Z_MS2_PIN          -1
+
+#define HEATER_BED_PIN      8
+#define TEMP_BED_PIN       14  //A14
+#define HEATER_0_PIN       10
+#define TEMP_0_PIN         13  //A13
+
+#define HEATER_1_PIN       -1 
+
+#ifdef PINDA_THERMISTOR //PINDA_THERMISTOR 
+  #define TEMP_1_PIN       15  //A15
+  #define TEMP_PINDA_PIN   15  //A15
+#else
+  #define TEMP_1_PIN       -1
+#endif 
+
+#ifdef AMBIENT_THERMISTOR //AMBIENT_THERMISTOR 
+  #define TEMP_1_PIN       15  //A15
+  #define TEMP_AMBIENT_PIN 15  //A15
+#else
+  #define TEMP_1_PIN       -1
+#endif 
+
+#define HEATER_2_PIN       -1
+#define TEMP_2_PIN         -1  
+
+#define E0_STEP_PIN        26
+#define E0_DIR_PIN         28
+#define E0_ENABLE_PIN      24
+#define E0_MS1_PIN         -1
+#define E0_MS2_PIN         -1
+#ifdef TMC2130
+  #define E0_TMC2130_CS    42
+  #define E0_TMC2130_DIAG  -1 //NO DIAG OUTPUTS
+#endif
+
+//#define MOTOR_CURRENT_PWM_XY_PIN -1
+//#define MOTOR_CURRENT_PWM_Z_PIN  -1
+//#define MOTOR_CURRENT_PWM_E_PIN  -1
+
+#define SDPOWER            -1
+#define SDSS               53  //The SDSS pin uses a different pin mapping from file Sd2PinMap.h
+#define LED_PIN            -1  //TODO what is this for?
+#define FAN_PIN             9
+#define FAN_1_PIN          -1
+#define PS_ON_PIN          12
+#define KILL_PIN           41  //80 with Smart Controller LCD
+#define SUICIDE_PIN        -1  //PIN that has to be turned on right after start, to keep power flowing.
+
+//#define LCD_BL_PIN         -1  //backlight control pin
+#define BEEPER             37
+#define LCD_PINS_RS        16
+#define LCD_PINS_ENABLE    17
+#define LCD_PINS_D4        23
+#define LCD_PINS_D5        25
+#define LCD_PINS_D6        27
+#define LCD_PINS_D7        29
+
+//buttons are directly attached using AUX-2
+#define BTN_EN1            33
+#define BTN_EN2            31
+#define BTN_ENC            35  // the click
+
+#ifndef SDSUPPORT
+  // these pins are defined in the SD library if building with SD support
+  #define SCK_PIN          52
+  #define MISO_PIN         50
+  #define MOSI_PIN         51
+#endif
+  
+#define SDCARDDETECT       49
+
+#define TACH_0             -1 
+#define TACH_1             -1 
+
+#ifndef IR_SENSOR
+  #define IR_SENSOR_PIN     5  //IR sensor on RAMPS pin D5 of servo motors pins
+#endif 
+
+// The Z2 axis, if any, should be the next open extruder port
+#ifdef Z_DUAL_STEPPER_DRIVERS
+    #define Z2_STEP_PIN    36
+    #define Z2_DIR_PIN     34
+    #define Z2_ENABLE_PIN  30
+#endif
